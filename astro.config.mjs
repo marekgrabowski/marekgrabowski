20ec7eae1e5f12e6c192e0993c68ad1bb6860dd4
marekgrabowski <marekg173@gmail.com>
2023-08-16 09:35:08 -0400

    skillsbage change

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
-
 import mdx from "@astrojs/mdx";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
     drafts: true
   },
-  integrations: [tailwind(), image(), mdx({
+  integrations: [tailwind(), mdx({
     drafts: true
-  })]
+  }), preact()],
+  experimental: {
+    assets: true
+  }
 });
