a15fbf1f4c310606aec897fe598c751f95549d1a
marekgrabowski <marekg173@gmail.com>
2023-08-16 11:27:48 -0400

    SVG logos hosted locally & astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 import { defineConfig } from 'astro/config';
+import compress from "astro-compress";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-
-import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
     drafts: true
   },
-  integrations: [tailwind(), mdx({
+  integrations: [tailwind(), compress(), mdx({
     drafts: true
   })],
   experimental: {
--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,37 +1,37 @@
 const CSTech = [
     {
         name: 'Astro',
-        img: 'https://bestofjs.org/logos/astro.dark.svg',
+        img: '/logos/astro-framework.svg',
         img_alt: 'Astro Logo',
         link: 'https://astro.build/',
     },
     {
         name: 'Python',
-        img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/768px-Python-logo-notext.svg.png',
+        img: '/logos/Python-logo-notext.svg',
         img_alt: 'Python Logo',
         link: 'https://www.python.org/',
     },
     {
         name: 'Tailwind CSS',
-        img: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Tailwind_CSS_Logo.svg',
+        img: '/logos/Tailwind_CSS_Logo.svg',
         img_alt: 'Tailwind CSS Logo',
         link: 'https://tailwindcss.com/',
     },
     {
         name: 'Java',
-        img: 'https://docs.graderthan.com/images/icons/product/java-dark.svg',
+        img: '/logos/java-icon.svg',
         img_alt: 'Java Logo',
         link: 'https://www.java.com/en/',
     },
     {
         name: 'C',
-        img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/The_C_Programming_Language_logo.svg/1280px-The_C_Programming_Language_logo.svg.png',
+        img: 'logos/C_Programming_Language.svg',
         img_alt: 'C Logo',
         link: 'https://www.iso.org/standard/74528.html',
     },
     {
         name: 'Kotlin',
-        img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Kotlin_logo_2021.svg/2560px-Kotlin_logo_2021.svg.png',
+        img: '/logos/Kotlin_Icon.svg',
         img_alt: 'Kotlin Logo',
         link: 'https://tailwindcss.com/',
     },
@@ -39,37 +39,37 @@ const CSTech = [
 const AVTech = [
     {
         name: 'Dante',
-        img: 'https://www.esi-audio.com/images/logo/dante.svg',
+        img: '/logos/danteav-vector-logo.svg',
         img_alt: 'Dante Logo',
         link: 'https://www.audinate.com/meet-dante',
     },
     {
         name: 'OBS',
-        img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/OBS_Studio_Logo.svg/1280px-OBS_Studio_Logo.svg.png',
+        img: '/logos/OBS_Studio_Logo.svg',
         img_alt: 'OBS Logo',
         link: 'https://obsproject.com/',
     },
     {
         name: 'Blackmagic ATEM ',
-        img: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/b8/Blackmagic_Design_logo.svg/2560px-Blackmagic_Design_logo.svg.png',
+        img: '/logos/blackmagic-design-vector-logo.svg',
         img_alt: 'Blackmagic Logo',
         link: 'https://www.blackmagicdesign.com/',
     },
     {
         name: 'Behringer Mixers ',
-        img: 'https://upload.wikimedia.org/wikipedia/en/d/d8/Behringer_logo_2010.png',
+        img: '/logos/behringer.svg',
         img_alt: 'Behringer Logo',
         link: 'https://www.behringer.com/',
     },
     {
         name: 'A&H Mixers ',
-        img: 'https://www.allen-heath.com/media/allen-and-heath-logo.png',
+        img: '/logos/allen-heath-vector-logo.svg',
         img_alt: 'Blackmagic Logo',
         link: 'https://www.allen-heath.com/',
     },
     {
         name: 'Adobe CC ',
-        img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Adobe_Creative_Cloud_rainbow_icon.svg/1280px-Adobe_Creative_Cloud_rainbow_icon.svg.png',
+        img: '/logos/Adobe_Creative_Cloud_rainbow_icon.svg',
         img_alt: 'Adobe Creative Cloud Logo',
         link: 'https://www.adobe.com/creativecloud.html',
     },
@@ -77,27 +77,27 @@ const AVTech = [
 const SBTech = [
     {
         name: 'Networking',
-        img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Wifi.svg/800px-Wifi.svg.png',
+        img: 'https://upload.wikimedia.org/wikipedia/commons/9/9e/Wifi.svg',
         img_alt: 'Wireless Networking Symbol',
     },
     {
         name: 'Automation',
-        img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Wifi.svg/800px-Wifi.svg.png',
+        img: 'https://upload.wikimedia.org/wikipedia/commons/9/9e/Wifi.svg',
         img_alt: 'Wireless Networking Symbol',
     },
     {
         name: 'Security Solutions',
-        img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Wifi.svg/800px-Wifi.svg.png',
+        img: 'https://upload.wikimedia.org/wikipedia/commons/9/9e/Wifi.svg',
         img_alt: 'Security Solutions Symbol',
     },
     {
         name: 'Web Design',
-        img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Wifi.svg/800px-Wifi.svg.png',
+        img: 'https://upload.wikimedia.org/wikipedia/commons/9/9e/Wifi.svg',
         img_alt: 'Wireless Networking Symbol',
     },
     {
         name: 'Social Media',
-        img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Wifi.svg/800px-Wifi.svg.png',
+        img: 'https://upload.wikimedia.org/wikipedia/commons/9/9e/Wifi.svg',
         img_alt: 'Wireless Networking Symbol',
     },
     
