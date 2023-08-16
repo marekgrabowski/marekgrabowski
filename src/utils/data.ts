faea42ec73c1f8d631ccbbe94b2a4427f0117fe6
Marek <46285102+marekgrabowski@users.noreply.github.com>
2023-08-15 21:33:17 -0400

    Updated Home page

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -40,8 +40,38 @@ const AVTech = [
     {
         name: 'Dante',
         imgSrc: 'https://www.esi-audio.com/images/logo/dante.svg',
-        altSrc: 'Astro Logo',
-        link: 'https://astro.build/',
+        altSrc: 'Dante Logo',
+        link: 'https://www.audinate.com/meet-dante',
+    },
+    {
+        name: 'OBS',
+        imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/OBS_Studio_Logo.svg/1280px-OBS_Studio_Logo.svg.png',
+        altSrc: 'OBS Logo',
+        link: 'https://obsproject.com/',
+    },
+    {
+        name: 'Blackmagic ATEM ',
+        imgSrc: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/b8/Blackmagic_Design_logo.svg/2560px-Blackmagic_Design_logo.svg.png',
+        altSrc: 'Blackmagic Logo',
+        link: 'https://www.blackmagicdesign.com/',
+    },
+    {
+        name: 'Behringer Mixers ',
+        imgSrc: 'https://upload.wikimedia.org/wikipedia/en/d/d8/Behringer_logo_2010.png',
+        altSrc: 'Behringer Logo',
+        link: 'https://www.behringer.com/',
+    },
+    {
+        name: 'A&H Mixers ',
+        imgSrc: 'https://www.allen-heath.com/media/allen-and-heath-logo.png',
+        altSrc: 'Blackmagic Logo',
+        link: 'https://www.allen-heath.com/',
+    },
+    {
+        name: 'Adobe CC ',
+        imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Adobe_Creative_Cloud_rainbow_icon.svg/1280px-Adobe_Creative_Cloud_rainbow_icon.svg.png',
+        altSrc: 'Adobe Creative Cloud Logo',
+        link: 'https://www.adobe.com/creativecloud.html',
     },
 ]
 const SBTech = [
@@ -49,7 +79,38 @@ const SBTech = [
         name: 'Networking',
         imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Wifi.svg/800px-Wifi.svg.png',
         altSrc: 'Wireless Networking Symbol',
-        link: 'https://astro.build/',
+        link: '',
     },
+    {
+        name: 'Automation',
+        imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Wifi.svg/800px-Wifi.svg.png',
+        altSrc: 'Wireless Networking Symbol',
+        link: '',
+    },
+    {
+        name: 'Security Solutions',
+        imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Wifi.svg/800px-Wifi.svg.png',
+        altSrc: 'Security Solutions Symbol',
+        link: '',
+    },
+    {
+        name: 'Web Design',
+        imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Wifi.svg/800px-Wifi.svg.png',
+        altSrc: 'Wireless Networking Symbol',
+        link: '',
+    },
+    {
+        name: 'Web Design',
+        imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Wifi.svg/800px-Wifi.svg.png',
+        altSrc: 'Wireless Networking Symbol',
+        link: '',
+    },
+    {
+        name: 'Web Design',
+        imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Wifi.svg/800px-Wifi.svg.png',
+        altSrc: 'Wireless Networking Symbol',
+        link: '',
+    },
+    
 ]
 export { CSTech, AVTech, SBTech, socials }
