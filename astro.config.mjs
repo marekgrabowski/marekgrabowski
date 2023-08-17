de431a9ea079b4341421b2158fb8006cc1b61f1b
Marek <46285102+marekgrabowski@users.noreply.github.com>
2023-08-16 21:28:13 -0400

    Robots.txt & Sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,20 @@ import compress from "astro-compress";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import preact from "@astrojs/preact";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
     drafts: true
   },
-  integrations: [preact({ compat: true }), tailwind(), compress(), mdx({
+  integrations: [preact({
+    compat: true
+  }), tailwind(), compress(), mdx({
     drafts: true
-  })],
+  }), sitemap()],
   experimental: {
     assets: true
-   }
- 
+  },
+  site: 'https://marekgrabowski.net',
 });
